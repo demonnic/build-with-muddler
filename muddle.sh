8a65625ce134c35c9e678ef8ee7e0dc11658b219
Damian Monogue <demonnic@gmail.com>
2021-03-19 21:58:44 -0400

    Remove dash from input name

--- muddle.sh
+++ muddle.sh
@@ -1,8 +1,8 @@
 cd $GITHUB_WORKSPACE
-curl -L https://github.com/demonnic/muddler/releases/download/0.5/muddle-shadow-$INPUT_MUDDLER-VERSION.tar --output muddler.tar
+curl -L https://github.com/demonnic/muddler/releases/download/0.5/muddle-shadow-$INPUT_MUDDLERVERSION.tar --output muddler.tar
 mkdir muddler-tmp
 tar xf muddler.tar -C muddler-tmp
-mv muddler-tmp/muddle-shadow-$INPUT_MUDDLER-VERSION muddler
+mv muddler-tmp/muddle-shadow-$INPUT_MUDDLERVERSION muddler
 rm -rf muddler-tmp muddler.tar
 muddler/bin/muddle
 rm -rf muddler
